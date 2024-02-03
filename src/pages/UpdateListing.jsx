154692edabf6e4b2f2f15e60445a5a24a0b92c83
unknown <nderim258@gmail.com>
2024-02-03 22:46:46 +0100

    Created listing card

--- src/pages/UpdateListing.jsx
+++ src/pages/UpdateListing.jsx
@@ -375,7 +375,7 @@ const UpdateListing = () => {
             disabled={loading || uploading}
             className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
           >
-            {loading ? "Loading..." : "Update Listing"}
+            {loading ? "Updating..." : "Update Listing"}
           </button>
           {error && <p className="text-red-700 text-sm">{error}</p>}
         </div>
