d84d1b40bd1d71860067910e68f93eaeb39c55a3
unknown <nderim258@gmail.com>
2024-02-05 10:39:59 +0100

    Created Home page

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -4,7 +4,7 @@ import { MdLocationOn } from "react-icons/md";
 
 const ListingCard = ({ listing }) => {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full xl:w-[24%] md:w-[48%]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={
